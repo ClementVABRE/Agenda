5ca5240f1f75e72e8041a15dd77b3106b48a95bf
clement vabre <clement.vabre@saintmichelannecy.fr>
2024-02-15 18:10:43 +0100

    Ajoutez des fichiers projet.

---
+++ calendrier/view/view_calendrier.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace calendrier.view
+{
+    /// <summary>
+    /// Logique d'interaction pour view_calendrier.xaml
+    /// </summary>
+    public partial class view_calendrier : UserControl
+    {
+        public view_calendrier()
+        {
+            InitializeComponent();
+            // Abonnez-vous à l'événement ScriptErrorsSuppressedChanged pour intercepter les erreurs de script
+            webBrowser.Navigating += WebBrowser_Navigating;
+        }
+        private void WebBrowser_Navigating(object sender, NavigatingCancelEventArgs e)
+        {
+            // Inspectez le contenu de la page ici
+            // Vous pouvez rechercher des erreurs de script ou d'autres problèmes
+            // Par exemple, vous pouvez utiliser JavaScript pour inspecter la page
+        }
+
+    }
+}
---
+++ calendrier/view/view_dashboard.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace calendrier.view
+{
+    /// <summary>
+    /// Logique d'interaction pour view_dashboard.xaml
+    /// </summary>
+    public partial class view_dashboard : UserControl
+    {
+        public view_dashboard()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void BTN_Contact_Click(object sender, RoutedEventArgs e)
+        {
+            var contactview = new view_contact(); // Assurez-vous de remplacer DashboardView par le nom de votre classe de vue du tableau de bord
+
+            // Remplace le contenu des deux parties de la grille
+            Ecran_Principale.Children.Clear(); // Efface tout contenu existant dans la grille
+            Grid.SetColumnSpan(contactview, 2);
+            Ecran_Principale.Children.Add(contactview);
+        }
+
+        private void BTN_Calendrier_Click(object sender, RoutedEventArgs e)
+        {
+            var calendrierview = new view_calendrier(); // Assurez-vous de remplacer DashboardView par le nom de votre classe de vue du tableau de bord
+            Ecran_Principale.Children.Clear(); // Efface tout contenu existant dans la grille
+            Grid.SetColumnSpan(calendrierview, 2);
+            Ecran_Principale.Children.Add(calendrierview);
+
+        }
+    }
+}
